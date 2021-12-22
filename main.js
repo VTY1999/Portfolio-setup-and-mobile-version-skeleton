cf5708eacf769ff2139918e88463db1f8995b4cd
verissimo francis <verissimo francis>
2021-12-22 13:44:22 +0100

    Make popup window active

--- main.js
+++ main.js
@@ -25,3 +25,28 @@ for (let i = 0; i < mobileMenuLinks.length; i += 1) {
 closeTab.addEventListener('click', () => {
   closeMenu();
 });
+
+const blurPop = document.querySelector('#blur');
+const popUp = document.querySelector('.popup');
+const seeProject = document.querySelector('.btn');
+
+function Toogle() {
+  blurPop.style.filter = 'blur(24px)';
+  popUp.style.display = 'block';
+}
+
+function closePopUp() {
+  popUp.style.display = 'none';
+  blurPop.style.filter = 'blur(0px)';
+}
+
+seeProject.addEventListener('click', () => {
+  Toogle();
+});
+
+const seeProjectLinks = document.querySelectorAll('.closePopUp');
+for (let i = 0; i < seeProjectLinks.length; i += 1) {
+  seeProjectLinks[i].addEventListener('click', () => {
+    closePopUp();
+  });
+}
