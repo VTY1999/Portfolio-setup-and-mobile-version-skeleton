fad28a1ca3e00a7b834a6a0f75c11694f55e2c70
verissimo francis <verissimotoyin50@gmail.com>
2022-07-19 15:58:10 +0100

    Update to JavaScript

--- main.js
+++ main.js
@@ -38,20 +38,80 @@ const projects = [{
 
 const blurPop = document.querySelector('#blur');
 const popUp = document.querySelector('.popup');
-const seeProject = document.querySelector('.btn');
+const popUp1 = document.querySelector('.popup1');
+const popUp2 = document.querySelector('.popup2');
+const popUp3 = document.querySelector('.popup3');
+const popUp4 = document.querySelector('.popup4');
+const popUp5 = document.querySelector('.popup5');
+const seeProject = document.querySelector('.botn');
+const seeProject1 = document.querySelector('.btn1');
+const seeProject2 = document.querySelector('.btn2');
+const seeProject3 = document.querySelector('.btn3');
+const seeProject4 = document.querySelector('.btn4');
+const seeProject5 = document.querySelector('.btn5');
 
 function Toogle() {
   blurPop.style.filter = 'blur(10px)';
   popUp.style.display = 'block';
 }
 
+function Toogle1() {
+  blurPop.style.filter = 'blur(10px)';
+  popUp1.style.display = 'block';
+}
+
+function Toogle2() {
+  blurPop.style.filter = 'blur(10px)';
+  popUp2.style.display = 'block';
+}
+
+function Toogle3() {
+  blurPop.style.filter = 'blur(10px)';
+  popUp3.style.display = 'block';
+}
+
+function Toogle4() {
+  blurPop.style.filter = 'blur(10px)';
+  popUp4.style.display = 'block';
+}
+
+function Toogle5() {
+  blurPop.style.filter = 'blur(10px)';
+  popUp5.style.display = 'block';
+}
+
 function closePopUp() {
   popUp.style.display = 'none';
+  popUp1.style.display = 'none';
+  popUp2.style.display = 'none';
+  popUp3.style.display = 'none';
+  popUp4.style.display = 'none';
+  popUp5.style.display = 'none';
   blurPop.style.filter = 'blur(0px)';
 }
 
 seeProject.addEventListener('click', () => {
   Toogle();
+});
+
+seeProject1.addEventListener('click', () => {
+  Toogle1();
+});
+
+seeProject2.addEventListener('click', () => {
+  Toogle2();
+});
+
+seeProject3.addEventListener('click', () => {
+  Toogle3();
+});
+
+seeProject4.addEventListener('click', () => {
+  Toogle4();
+});
+
+seeProject5.addEventListener('click', () => {
+  Toogle5();
 });
 
 const seeProjectLinks = document.querySelectorAll('.closePopUp');
